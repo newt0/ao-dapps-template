85b0624517be6432c7356c74edbaf04ea703b904
newt0 <py_lambda@yahoo.co.jp>
2025-09-08 17:05:44 +0900

    fix

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,5 +1,5 @@
-export { AOButton } from './ao-button';
-export { WalletButton } from './wallet-button';
+export { default as AoButton } from './ao-button';
+export { default as WalletButton } from './wallet-button';
 export { MetricItem } from './metric-item';
 export { BalanceDisplay } from './balance-display';
 export { SectionCard } from './section-card';
